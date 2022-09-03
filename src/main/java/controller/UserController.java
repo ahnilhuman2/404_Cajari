ec1856df43cb5907ca09c646276640e55a4278d5
jinsulog1 <ahtm0467@gmail.com>
2022-09-04 01:31:17 +0900

    B#5
    Usercontroller > Switch 생성 / uri 코드 / viewPage forward 코드 / views>user>login.jsp 생성

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,15 +14,47 @@ public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     public UserController() {
-    	
+    	super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		action(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		request.setCharacterEncoding("UTF-8");
+		action(request, response);
 	}
+	
+	protected void action(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("\n>> " + getClass().getName() + ". 호출");
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String command = uri.substring(conPath.length());
+		
+		String method = request.getMethod();
+		
+		System.out.println("request: " + uri + " (" + method + ")");  
+		System.out.println("conPath: " + conPath);  
+		System.out.println("command: " + command);
+		
+//		Service service = null;
+		String viewPage = null;
+		
+		switch(command) {
+		case "/user/login":
+			viewPage = "login.jsp";
+			break;
+		} // end switch
+		
+		if(viewPage != null) {
+			RequestDispatcher dispatcher = 
+					request.getRequestDispatcher("/WEB-INF/views/user/" + viewPage);
+			
+			dispatcher.forward(request, response);
+		}
+		
+	} // end action
 
-}
+} // end Controller
