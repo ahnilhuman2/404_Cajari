410991ab65d80f046826fc19f60ec7e57b276111
jinsulog1 <ahtm0467@gmail.com>
2022-09-04 20:32:04 +0900

    B#7
    ReviewController 생성 / views > reivew > list.jsp 생성

---
+++ src/main/java/controller/ReviewController.java
@@ -0,0 +1,60 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/review/*")
+public class ReviewController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public ReviewController() {
+    	super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		action(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		action(request, response);
+	}
+	
+	protected void action(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("\n>> " + getClass().getName() + ". 호출");
+		
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String command = uri.substring(conPath.length());
+		
+		String method = request.getMethod();
+		
+		System.out.println("request: " + uri + " (" + method + ")");  
+		System.out.println("conPath: " + conPath);  
+		System.out.println("command: " + command);
+		
+//		Service service = null;
+		String viewPage = null;
+		
+		switch(command) {
+		case "/review/list":
+			viewPage = "list.jsp";
+			break;
+		} // end switch
+		
+		if(viewPage != null) {
+			RequestDispatcher dispatcher = 
+					request.getRequestDispatcher("/WEB-INF/views/review/" + viewPage);
+			
+			dispatcher.forward(request, response);
+		} 
+		
+	} // end action
+
+} // end Controller
