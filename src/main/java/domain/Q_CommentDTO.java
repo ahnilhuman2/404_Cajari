44d96b882461578585783203071ed68e15b1af14
jinsulog1 <ahtm0467@gmail.com>
2022-09-07 13:02:15 +0900

    B#17
    domain 수정

---
+++ src/main/java/domain/Q_CommentDTO.java
@@ -0,0 +1,52 @@
+package domain;
+
+import java.time.LocalDateTime;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Q_CommentDTO {
+	private int id;
+	private String content;
+	
+	@JsonDeserialize(using = LocalDateTimeDeserializer.class)
+	@JsonSerialize(using = LocalDateTimeSerializer.class)
+	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+	@JsonProperty("regdate")
+	private LocalDateTime regDate;
+	private UserDTO user_id;      // user_id (작성자)
+	
+	@JsonIgnore
+	private Qna_WriteDTO qna_id;    // qna_write_id(외래키)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
