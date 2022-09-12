272f4ef4d73d287ebb8625cd8d3da2cb98ebcc47
suminLee <lsm6421@naver.com>
2022-09-13 05:59:00 +0900

    QnAComment
    mapper연결

--- src/main/java/controller/QnACommentController.java
+++ src/main/java/controller/QnACommentController.java
@@ -1,5 +1,26 @@
 package controller;
 
-public class QnACommentController {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/qnacomment/*")
+public class QnACommentController {
+	private static final long serialVersionUID = 1L;
+	
+	public QnACommentController() {
+        super();
+    }
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		action(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");   
+		action(request, response);
+	}
 }
